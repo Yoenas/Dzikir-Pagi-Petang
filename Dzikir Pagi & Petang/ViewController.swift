3e3c6470d4e66a826cd74304c7f28ae3b521e2ff
Yoenas <yoenas@yoenass-macbook-pro.local>
2018-10-10 10:27:08 +0700

    Initial Commit

---
+++ Dzikir Pagi & Petang/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dzikir Pagi & Petang
+//
+//  Created by Yoenas on 10/10/18.
+//  Copyright © 2018 Yoenas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
